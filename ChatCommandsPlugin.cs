17b193f18fdc830577700bd07bf971025b4a8090
Vis'Eyth <viseyth_dev@protonmail.com>
2019-04-19 10:02:28 +0200

    Status response update - v1.1.0
    Now executing the console command _all by ourselves_, therefore catching
    it ourselves, too. Most of the code rewritten.

--- ChatCommandsPlugin.cs
+++ ChatCommandsPlugin.cs
@@ -23,15 +23,16 @@
  */
 
 using System.Collections;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using BepInEx;
 using RoR2;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace ChatCommands
 {
-    // ReSharper disable once StringLiteralTypo
-    [BepInPlugin("com.viseyth.ror2.chatcommands", "ChatCommands", "1.0.0")]
+    [BepInPlugin("com.viseyth.ror2.chatcommands", "ChatCommands", "1.1.0")]
     public class ChatCommandsPlugin : BaseUnityPlugin
     {
         private Console _console;
@@ -47,67 +48,127 @@ namespace ChatCommands
             };
             On.RoR2.Chat.SendBroadcastChat_ChatMessageBase += (orig, message) =>
             {
-                if (!(message is Chat.UserChatMessage msg && TryCommand(msg)))
+                if (message is Chat.UserChatMessage msg && IsCommandSyntax(msg.text))
+                {
+                    Chat.AddMessage(msg.ConstructChatString());
+                    ExecuteCommand(msg);
+                }
+                else
                     orig(message);
             };
         }
 
-        private bool TryCommand(Chat.UserChatMessage message)
+        private void ExecuteCommand(Chat.UserChatMessage message)
         {
-            if (!ParseCommand(message.text, out var command, out var callString))
-                return false;
+            var callString = GetCommand(message.text);
+            if (callString.Length == 0)
+                return;
 
             var user = message.sender.GetComponent<NetworkUser>();
-            Debug.Log(user.userName + " called command \"" + command + "\" using chat with \"" + callString + "\".");
+            var args = ParseCommand(callString, message.sender);
 
-            if (!_catalog.Contains(command))
+            Debug.Log(user.userName + " called command \"" + args[0] + "\" using chat with \"" +
+                      string.Join(" ", args) + "\".");
+
+            var cmd = args[0];
+            args.RemoveAt(0);
+
+            try
             {
-                SendResponse("Command \"" + command + "\" is not registered.", true);
-                return true;
+                if (!_catalog.Contains(cmd))
+                    throw new ConCommandException("Command is not registered.");
+
+                var flags = _catalog[cmd].GetFieldValue<ConVarFlags>("flags");
+
+                if ((flags & ConVarFlags.SenderMustBeServer) != ConVarFlags.None &&
+                    !NetworkServer.active)
+                    throw new ConCommandException("Command is only usable by the host.");
+
+                if ((flags & ConVarFlags.Cheat) != ConVarFlags.None && !RoR2Application.cvCheats.boolValue)
+                    throw new ConCommandException("Command cannot be used while cheats are disabled.");
+
+                if ((flags & ConVarFlags.ExecuteOnServer) != ConVarFlags.None &&
+                    !NetworkServer.active)
+                {
+                    _console.InvokeMethod("ForwardCmdToServer", new ConCommandArgs
+                    {
+                        sender = user,
+                        commandName = cmd,
+                        userArgs = args
+                    });
+                    ShowResponse($"Command \"{cmd}\" sent to server.");
+                    return;
+                }
+
+                var command = _catalog[cmd].GetFieldValue<Console.ConCommandDelegate>("action");
+
+                command(new ConCommandArgs
+                {
+                    sender = user,
+                    commandName = cmd,
+                    userArgs = args
+                });
+
+                ShowResponse($"Command \"{cmd}\" executed successfully.");
             }
-
-            var flags = _catalog[command].GetFieldValue<ConVarFlags>("flags");
-
-            if ((flags & ConVarFlags.Cheat) != ConVarFlags.None && !RoR2Application.cvCheats.boolValue)
+            catch (ConCommandException ex)
             {
-                SendResponse("Command \"" + command + "\" cannot be used while cheats are disabled.", true);
-                return true;
+                ShowResponse($"Command \"{cmd}\" failed: {ex.Message}", true);
             }
-
-            if ((flags & ConVarFlags.SenderMustBeServer) != ConVarFlags.None &&
-                !UnityEngine.Networking.NetworkServer.active)
-            {
-                SendResponse("Command \"" + command + "\" is only usable by the host.", true);
-                return true;
-            }
-
-            _console.SubmitCmd(user, callString.Substring(1));
-            SendResponse("Executed \"" + callString + "\".", false);
-
-            return true;
         }
 
-        private static void SendResponse(string response, bool error)
+        private static void ShowResponse(string response, bool error = false)
         {
             var message = "<color=#" + (error ? "f08080" : "98fb98") + ">System: " + response + "</color>";
             Chat.AddMessage(message);
         }
 
-        private static readonly Regex CommandRegex = new Regex(@"^\/([\d\w]+)(?: | .+?)*(?:$|;)");
+        private static readonly Regex CommandRegex = new Regex(@"^\/([\d\w]+(?: | .+?)*)(?:$|;)");
 
-        private static bool ParseCommand(string input, out string command, out string call)
+        private static bool IsCommandSyntax(string input) => CommandRegex.IsMatch(input);
+
+        private static string GetCommand(string input) =>
+            !IsCommandSyntax(input) || CommandRegex.Match(input).Groups.Count < 2
+                ? ""
+                : CommandRegex.Match(input).Groups[1].Value.Trim();
+
+        private List<string> ParseCommand(string command, GameObject sender)
         {
-            var match = CommandRegex.Match(input);
-            if (match.Success)
-            {
-                command = match.Groups[1].Value;
-                call = match.Groups[0].Value;
+            var lexer = Reflection
+                .GetNestedType<Console>("Lexer")
+                .GetConstructor(new[] {typeof(string)})?
+                .Invoke(new object[] {command});
 
-                return true;
+            var tokens = lexer.InvokeMethod<Queue<string>>("GetTokens");
+
+            var args = new List<string>();
+            var vstrFlag = false;
+
+            while (tokens.Count != 0)
+            {
+                var token = tokens.Dequeue();
+                if (token == ";")
+                {
+                    vstrFlag = false;
+                    if (args.Count > 0)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if (vstrFlag)
+                    {
+                        token = _console.InvokeMethod<string>("GetVstrValue", sender, token);
+                        vstrFlag = false;
+                    }
+
+                    if (token == "vstr") vstrFlag = true;
+                    else args.Add(token);
+                }
             }
 
-            command = call = "";
-            return false;
+            return args;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-### ChatCommands - BepInEx plugin for commands via chat
+## ChatCommands - BepInEx plugin for commands via chat
 
-If you've ever had to use a single console command a lot during a game, im sure you hated opening the console too.
+If you've ever had to use console commands a lot during a game, im sure you hated opening the console too.
 
 So, here it is.
 
-Console commands, per chat. (That's all it does, i promise)
+Console commands, via chat. (That's all it does, I promise)
 
-#### What works:
-#####Calling registered commands
+### What works:
+#### Calling registered commands
 This plugin checks for registered console commands, and if the command is registered, runs the command via call to the
 console. So, as long as your favourite mod adds actual console commands, this mod will work with it with no extra
 configuration required.
 
-#####Status response
-This is not the actual console response, but the response from a pre-execution check. Still, you get a response if I
-think you should be able to run the command (based on flags).
+#### Status response (since 1.1.0)
+Now calling and catching console functions by _itself_, will now actually show you the exception message if something
+went wrong. On the other side, since that means i had to walk the whole mile myself, this means the plugin will probably
+need attention on new updates.
 
-#### What doesn't (yet):
-#####Console output in chat
-If i ever find a nice solution to this problem, it will be added.
+### What doesn't (yet):
+#### Console output in chat
+If I ever find a nice solution to this problem, it will be added.
